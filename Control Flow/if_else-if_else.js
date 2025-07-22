e3bd977189823852bae8ddddaefa640eab80dfb4
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-22 09:44:36 +0530

    Code executed of if_else-if_else

---
+++ Control Flow/if_else-if_else.js
@@ -0,0 +1,63 @@
+// Control Flow :- 
+/*
+Control flow is the order in which the computer executes statements in a script. In JavaScript, control flow is used to manage the execution path based on different conditions.
+*/
+
+
+let a = 10 ; 
+let b = 5 ; 
+
+// If statement :- 
+if (a > b) {
+  console.log("a is greater than b");
+}
+
+
+// if_else 
+if (a > b) {
+  console.log("a is greater");
+} else {
+  console.log("b is greater or equal");
+}
+
+
+// else if 
+if (a > b) {
+  console.log("a is greater");
+} else if (a === b) {
+  console.log("a and b are equal");
+} else {
+  console.log("b is greater");
+}
+
+
+
+
+// Switch :- 
+let day = "Monday";
+
+switch (day) {
+  case "Monday":
+    console.log("Start of the week");
+    break;
+  case "Friday":
+    console.log("End of the week");
+    break;
+  default:
+    console.log("Midweek day");
+}
+
+
+
+// nested conditions 
+const balance = 1000 
+
+if(balance < 500) {
+    console.log("Less than") ;
+} else if(balance < 750) {
+    console.log("Less than 500") ;
+} else if(balance < 900) {
+    console.log("Less than 900") ;
+} else {
+    console.log("Less than 1200") ;
+}
