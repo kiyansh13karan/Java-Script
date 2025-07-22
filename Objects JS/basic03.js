57713b649b7ff233628ea7eb993a4b9a882c7c8b
Karan Nayal <somnathtigers2006@gmail.com>
2025-07-22 09:10:14 +0530

    Object de-structure

---
+++ Objects JS/basic03.js
@@ -0,0 +1,22 @@
+// Object de-structure 
+
+const person = {
+    coursename : "JS in hindi" , 
+    coursefees : "999" ,
+    courseInstructor : "Hitesh Chaudhary" ,
+}
+const {coursename : cn} = person ; 
+const {coursefees : cf} = person ; 
+const {courseInstructor : ci} = person ; 
+
+
+console.log(cn) ;
+console.log(cf) ;
+console.log(ci) ;
+
+
+
+
+
+
+
